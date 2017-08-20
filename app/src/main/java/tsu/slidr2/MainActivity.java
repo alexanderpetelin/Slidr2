b295d50b043f384809e2c511a5c24fe0e048f638
Alexander Petelin <aepetelin@gmail.com>
2017-08-20 21:20:17 +0700

    New changelist 2

---
+++ app/src/main/java/tsu/slidr2/MainActivity.java
@@ -0,0 +1,30 @@
+package tsu.slidr2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView1);
+        textView2 = (TextView) findViewById(R.id.textView2);
+
+        textView.setText("- Hello!");
+        textView2.setText("- Hello!");
+
+        textView.setText("- Hello! 2");
+        textView2.setText("- Hello! 2");
+
+        textView.setText("- Hello! 3");
+        textView2.setText("- Hello! 3");
+    }
+}
---
+++ app/src/main/java/tsu/slidr2/NewClass.java
@@ -0,0 +1,7 @@
+package tsu.slidr2;
+
+/**
+ * Created by DnS on 20.08.2017.
+ */
+public class NewClass {
+}
